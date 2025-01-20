808f37a4e19eb1c9693d508c46f0170bd00f21b5
Kasra Zafari <zafari.kasra@gmail.com>
2025-01-20 10:02:08 +0330

    create Header, Footer & HomePage components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 
 // import './App.css'
 
+import HomePage from "./components/HomePage"
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
+
 function App() {
 
   return (
     <>
       <h1>hello snapp</h1>
+      <Header/>
+      <HomePage/>
+      <Footer/>
     </>
   )
 }
