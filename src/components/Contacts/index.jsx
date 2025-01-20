ce1ce4cb8ca2740f2d4e04985f8264bbff32ec62
Kasra Zafari <zafari.kasra@gmail.com>
2025-01-20 12:46:06 +0330

    start contacts page

--- src/components/Contacts/index.jsx
+++ src/components/Contacts/index.jsx
@@ -3,7 +3,21 @@
 const Contacts = () => {
 
     return (
+        <>
+        <div>
         <h1>Contacts</h1>
+        <div>
+            <ul>
+                <li>
+                    item1
+                </li>
+                <li>
+                    item2
+                </li>
+            </ul>
+        </div>
+        </div>
+        </>
     )
 }
 
