4f74dcfd024db3780a63746604e530ddd4272c96
Kasra Zafari <zafari.kasra@gmail.com>
2025-01-20 20:35:34 +0330

    map/data.results

--- src/components/Contacts/index.jsx
+++ src/components/Contacts/index.jsx
@@ -1,36 +1,35 @@
 import { useState } from "react";
 import { useEffect } from "react";
+import { data } from "react-router-dom";
 
 
 const Contacts = () => {
 
-    const[contacts, setContacts]=useState([]);
-    console.log(contacts);
-    
+    const [contacts, setContacts] = useState([]);
 
-useEffect(()=>{
-    fetch("https://randomuser.me/api/?results=50")
-    .then(res=>res.json())
-    .then(date=>{
-        setContacts(date)
-    })
-},[])
+
+    useEffect(() => {
+        fetch("https://randomuser.me/api/?results=50")
+            .then(res => res.json())
+            .then(data => {
+                setContacts(data.results)
+            })
+    }, [])
 
     return (
         <>
-        <div>
-        <h1>Contacts</h1>
-        <div>
-            <ul>
-                <li>
-                    item1
-                </li>
-                <li>
-                    item2
-                </li>
-            </ul>
-        </div>
-        </div>
+            <div>
+                <h1>Contacts</h1>
+                <div>
+                    <ul>
+                        {contacts.map(contact => (
+                            <li>
+                                <img src={contact.picture.thumbnail} alt="pic" />
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
         </>
     )
 }
