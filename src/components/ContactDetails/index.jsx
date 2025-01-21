a4aeebbc6f30ca6ea575b4c05f5b8527feb2a243
Kasra Zafari <zafari.kasra@gmail.com>
2025-01-21 19:48:01 +0330

    Update ContactDetails

--- src/components/ContactDetails/index.jsx
+++ src/components/ContactDetails/index.jsx
@@ -1,19 +1,18 @@
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const ContactDetails = () => {
     const params = useParams();
     const [contact, setContact] = useState(null);
-    console.log(contact);
-    
+
 
     useEffect(() => {
-        if(params.id){
+        if (params.id) {
             fetch(`https://dummyjson.com/users/${params.id}`)
-            .then(res => res.json())
-            .then(data => {
-                setContact(data);
-            })
+                .then(res => res.json())
+                .then(data => {
+                    setContact(data);
+                })
         }
     }, [params.id]);
 
@@ -22,7 +21,15 @@ const ContactDetails = () => {
     };
 
     return (
-        <p>{contact.firstName}</p>
+        <div>
+            <img src={contact.image} alt="" />
+            <h2>{contact.firstName} {contact.lastName}</h2>
+            <p><strong>Phone:</strong> {contact.phone}</p>
+            <p><strong>Email:</strong> {contact.email}</p>
+            <p><strong>City:</strong> {contact.address.city}</p>
+            <p><strong>Address:</strong> {contact.address.address}</p>
+        </div>
+
     )
 }
 
